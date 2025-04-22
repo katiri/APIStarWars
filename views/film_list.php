23bff8983074e6bcccd5343c1d22e5efdbab3788
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2025-04-22 16:52:56 -0300

    Correção para funcionar em produção

--- views/film_list.php
+++ views/film_list.php
@@ -2,6 +2,10 @@
     if($http_code){
         http_response_code($http_code);
     }
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 
     header('Content-Type: application/json');
     
